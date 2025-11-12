84e237835be737da3ddc2c37f16229b9d2984953
Sahil Makhija <sahilmakhija667@gmail.com>
2025-11-12 13:45:58 +0530

    [New] Patterns 11-16

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -100,6 +100,81 @@ fn pattern_tenth(n: u32) {
     }
 }
 
+fn pattern_11(n: u32) {
+    for i in 0..n {
+        for j in 0..(i + 1) {
+            print!("{} ", ((i + j + 1) % 2));
+        }
+        print!("\n");
+    }
+}
+
+fn pattern_12(n: u32) {
+    for i in 0..(n - 1) {
+        for j in 1..n {
+            if j <= (i + 1) {
+                print!("{}", j);
+            } else {
+                print!(" ");
+            }
+        }
+        for j in 1..n {
+            if (n - j) > (i + 1) {
+                print!(" ");
+            } else {
+                print!("{}", n - j);
+            }
+        }
+        print!("\n");
+    }
+}
+
+fn pattern_13(n: u32) {
+    // 4 --> 10
+    // 3 --> 6
+    // 2 --> 3
+    // 1 --> 1
+    // 0 --> 0
+    let mut cf = 0;
+    for i in 0..n {
+        cf += i;
+        for j in 0..(i + 1) {
+            print!("{}", cf + j + 1);
+        }
+        print!("\n");
+    }
+}
+
+fn pattern_14(n: u8) {
+    // print!("{}", (b'A' + 4) as char);
+    for i in 0..n {
+        for j in 0..(i + 1) {
+            print!("{}", (b'A' + j) as char);
+        }
+        print!("\n");
+    }
+}
+
+fn pattern_15(n: u8) {
+    // print!("{}", (b'A' + 4) as char);
+    for i in 0..n {
+        for j in 0..(n - i) {
+            print!("{}", (b'A' + j) as char);
+        }
+        print!("\n");
+    }
+}
+
+fn pattern_16(n: u8) {
+    // print!("{}", (b'A' + 4) as char);
+    for i in 0..n {
+        for j in 0..(i + 1) {
+            print!("{}", (b'A' + i) as char);
+        }
+        print!("\n");
+    }
+}
+
 fn main() {
     // println!("Hello, world!");
     // print_square(8);
@@ -111,5 +186,11 @@ fn main() {
     // pattern_seventh(5);
     // pattern_eighth(5);
     // pattern_ninth(5);
-    pattern_tenth(5);
+    // pattern_tenth(5);
+    // pattern_11(5);
+    // pattern_12(5);
+    // pattern_13(5);
+    // pattern_14(5);
+    pattern_15(5);
+    // pattern_16(5);
 }
